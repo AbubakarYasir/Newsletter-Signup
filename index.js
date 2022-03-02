11b141fc773bef6b7a84e97bd0a087caae1201a1
AbubakarYasir <abubakaryasir569@gmail.com>
2022-03-02 22:56:16 +0500

    Main Page Design & HTML - Completed

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 const http = require("http");
 const port = 3000;
 
 const app = express();
 
+app.use(express.static("static"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {});
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/static/signup.html");
+});
+
+app.post("/", function (req, res) {
+    let firstName = req.body.fName;
+    let secondName = req.body.lName;
+    let email = req.body.email;
+    console.log(firstName, secondName, email);
+    res.sendFile(__dirname + "/static/success.html");
+});
 
 app.listen(port, function () {
-    console.log("Server is started and Running at http://localhost:" + port);
+    console.log("Server is started at http://localhost:" + port);
 });
