de1c61c81bd12887b694a8aae193902888d1979c
AbubakarYasir <abubakaryasir569@gmail.com>
2022-03-03 09:45:28 +0500

    Setting Up Post Request to API

--- index.js
+++ index.js
@@ -14,13 +14,27 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    let firstName = req.body.fName;
-    let secondName = req.body.lName;
-    let email = req.body.email;
-    console.log(firstName, secondName, email);
-    res.sendFile(__dirname + "/static/success.html");
-});
+    const firstName = req.body.fName;
+    const secondName = req.body.lName;
+    const email = req.body.email;
 
+    const apiData = {
+        members: [
+            {
+                email_address: email,
+                staturs: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: secondName,
+                },
+            },
+        ],
+    };
+
+    const jsonData = JSON.stringify(apiData);
+    console.log(jsonData);
+    const url = "https://us14.api.mailchimp.com/3.0/lists/857bdac0fc";
+});
 app.listen(port, function () {
     console.log("Server is started at http://localhost:" + port);
 });
